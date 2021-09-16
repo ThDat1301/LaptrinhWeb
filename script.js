21ca1d90714da0b77ad086909dac234db547a7f5
ThDat1301 <2051052028dat@ou.edu.vn>
2021-09-16 12:25:01 +0700

    Sep 16

--- script.js
+++ script.js
@@ -3,21 +3,19 @@ var span =document.getElementsByClassName("close")[0]
 var button = document.getElementsByClassName("Muahang")
 var imgProduct = document.getElementsByClassName("imgPro")
 var mainImg = document.querySelector("div.imgThumb img")
-var click = false;
-// When the user clicks the button, open the modal 
-for(var i = 0; i < button.length-1; i++)
+var mainDes = document.querySelector("div.modalContent div p")
+let des = document.getElementsByClassName("DesSp")
+
+// Modal description about product
+for(let i = 0; i < button.length; i++)
 {
-    
-    {button[i].onclick = function() {
-        /*for(var i = 0; i < imgProduct.length; i++)
-        {
-            var path = imgProduct[i].src;
-            mainImg.setAttribute("src",path)
-            modal.style.display = "block";
-        }*/
-        console.log(button[i])
-        click = true;
-    }}
+    button[i].onclick = function() {
+
+            let pathImg = imgProduct[i].src;
+            mainImg.setAttribute("src",pathImg)
+            mainDes.innerHTML = des[i].innerHTML
+            modal.style.display = "block"
+    }
 }
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
